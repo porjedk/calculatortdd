d61a0ac7abf2c63a5646e030886776488d0a75ab
Porjed Kuljittiyanee - TTH-Dev <p.kuljittiyanee@togetherteam.co.th>
2019-01-04 11:48:20 +0700

    Implement TestCalculator to test input 1,2 and return sum of them as 3

--- calculator_test.go
+++ calculator_test.go
@@ -11,7 +11,7 @@ func TestCalculator (t *testing.T) {
 		}
 	}
 
-	t.Run("Test Input Empty String Return 0", func(t *testing.T) {
+	t.Run("Test Input Empty String and Return 0", func(t *testing.T) {
 		got := Calculator("")
 		want := "0"
 		assertCorrectMessage(t, got, want)
@@ -20,6 +20,12 @@ func TestCalculator (t *testing.T) {
 	t.Run("Test Input 1 and Return 1", func(t *testing.T) {
 		got := Calculator("1")
 		want := "1"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Test Input 1,2 and Return sum of 1,2 as 3 ", func (t *testing.T) {
+		got := Calculator("1,2")
+		want := "3"
 		assertCorrectMessage(t, got, want)
 	})
 }
