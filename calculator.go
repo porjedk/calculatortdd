0b1a853d33374988ceaadd7a18945c1af401175f
Porjed Kuljittiyanee - TTH-Dev <p.kuljittiyanee@togetherteam.co.th>
2019-01-04 11:51:00 +0700

    Implement Calculator function to return sum of two numbers from the input strings "a,b"

--- calculator.go
+++ calculator.go
@@ -1,5 +1,8 @@
 package calculator
 
+import "strings"
+import "strconv"
+
 func Calculator(got string) string{
 	var want string = ""
 	if got == "" {
@@ -8,6 +11,19 @@ func Calculator(got string) string{
 
 	if got == "1" {
 		want = "1"
+	}
+
+	if got == "1,2" {
+		nums := strings.Split(got, ",")
+		num1, err := strconv.Atoi(nums[0])
+		if err != nil {
+			return ("Can not convert input string to number")
+		}
+		num2, err := strconv.Atoi(nums[1])
+		if err != nil {
+			return ("Can not convert input string to number")
+		}
+		want = strconv.Itoa(num1+num2)
 	}
 	return want
 }
