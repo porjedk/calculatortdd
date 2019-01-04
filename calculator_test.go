caff08da27a7de27fb94269c58c0e24d57719c72
Porjed Kuljittiyanee - TTH-Dev <p.kuljittiyanee@togetherteam.co.th>
2019-01-04 11:08:40 +0700

    Refactor TestCalculator function by added assertCorrectMessage method.

--- calculator_test.go
+++ calculator_test.go
@@ -3,20 +3,23 @@ package calculator
 import "testing"
 
 func TestCalculator (t *testing.T) {
-	t.Run("Test Input Empty String Return 0", func(t *testing.T) {
-		got := Calculator("")
-		want := "0"
 
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
+	}
+
+	t.Run("Test Input Empty String Return 0", func(t *testing.T) {
+		got := Calculator("")
+		want := "0"
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Test Input 1 and Return 1", func(t *testing.T) {
 		got := Calculator("1")
 		want := "1"
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
